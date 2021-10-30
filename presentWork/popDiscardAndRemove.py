e7ea6e94c9a290d1b0fde87769502325438375f0
Gaurav Mandali <Gaurav.Mandali@outlook.com>
2021-10-30 15:51:15 -0500

    These are the recent problems

---
+++ presentWork/popDiscardAndRemove.py
@@ -0,0 +1,14 @@
+NumOfElementsInSet = int(input())
+MainSet = set(map(int, input().split()))
+NumOfOperations = int(input())
+
+
+for i in range(NumOfOperations):
+    BigInput = input().split()
+    if BigInput[0] == "pop":
+        MainSet.pop()
+    elif BigInput[0] == "discard":
+        MainSet.discard(int(BigInput[1]))
+    elif BigInput[0] == "remove":
+        MainSet.remove(int(BigInput[1]))
+print(sum(MainSet))
