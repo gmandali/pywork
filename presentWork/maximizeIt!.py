497dfa10c06e8d42b3c2b61b7d5c7aeaf67c0c5f
Gaurav Mandali <Gaurav.Mandali@outlook.com>
2021-12-15 18:43:08 -0600

    this is the homework

---
+++ presentWork/maximizeIt!.py
@@ -0,0 +1,18 @@
+from itertools import repeat
+from itertools import product
+
+
+def myFunction(data, modula):
+    return sum(x * x for x in data) % modula
+
+
+a, b = list(map(int,input().split()))
+maximum = []
+for currentList in range(a):
+    something = list(map(int, input().split()[1:]))
+    maximum.append(something)
+
+
+result = list(product(*maximum))
+finalResult = list(map(myFunction, result, repeat(b)))
+print(max(finalResult))
