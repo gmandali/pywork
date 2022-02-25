0cd96284101b00efdfa34efc66b7431f0c7e8b15
Gaurav Mandali <Gaurav.Mandali@outlook.com>
2022-02-24 19:10:33 -0600

    homework

--- presentWork/evenExcercise.py
+++ presentWork/evenExcercise.py
@@ -1,8 +1,6 @@
-num1, num2 = input().split()
+start, end = map(int, input().split())
 numList = []
-newnum1 = int(num1)
-newnum2 = int(num2)
-for i in range(newnum1, newnum2 + 1):
+for i in range(start, end + 1):
     if i % 2 == 0:
         numList.append(i)
 
